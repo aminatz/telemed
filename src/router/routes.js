4f9ea6cb5ff3e2dc76eab143637c3c935326ee3c
Renato Acordo <51852407+aminatz@users.noreply.github.com>
2023-07-10 12:33:07 +0800

    third

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,18 +2,22 @@
 const routes = [
   {
     path: '/',
+    path: '/guest',
+    component: () => import('layouts/GuestLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/LoginPage.vue') },
+      { path: 'register', component: () => import('pages/RegistrationPage.vue') },
+    ]
+  },
+  {
+    path: '/auth',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: 'patients', component: () => import('pages/PatientsPage.vue') },
+      { path: 'appointments', component: () => import('pages/AppointmentsPage.vue') },
+      { path: 'chats', component: () => import('pages/ChatsPage.vue') },
+      { path: 'consultations', component: () => import('pages/ConsultationsPage.vue') },
     ]
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
-
-export default routes
