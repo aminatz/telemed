b139211ff68d24a70bedcde0ae07ee4e4f8251a5
Renato Acordo <51852407+aminatz@users.noreply.github.com>
2023-07-13 15:36:00 +0800

    firebase init

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,60 +3,75 @@ import {
     updateProfile,
     signInWithEmailAndPassword,
     signOut,
-  } from "firebase/auth";
-  import { auth } from "boot/firebaseConnection";
-  
-  export const getUser = () => {
+} from "firebase/auth";
+import { auth } from "boot/firebaseConnection";
+import { api } from "boot/axios";
+
+export const checkHospitalUser = () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user) {
+        api.get('/api/users/' + user.uid).then((res) => {
+            localStorage.setItem("hospital", JSON.stringify(true));
+        }).catch((err) => {
+            localStorage.setItem("hospital", JSON.stringify(false));
+        })
+    } else {
+        localStorage.setItem("hospital", JSON.stringify(false));
+    }
+}
+
+export const getUser = () => {
     return auth.currentUser;
-  };
-  
-  export const update = (user, data) =>
+};
+
+export const update = (user, data) =>
     new Promise((resolve, reject) => {
-      updateProfile(user, data);
-      resolve();
+        updateProfile(user, data);
+        resolve();
     });
-  
-  export const register = (data) =>
+
+export const register = (data) =>
     new Promise((resolve, reject) => {
-      const { email, password, ...rest } = data;
-  
-      createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          if (Object.keys(rest).length > 0) {
-            updateProfile(userCredential.user, rest);
-          }
-  
-          resolve(userCredential.user);
-        })
-        .catch((err) => {
-          reject(err.message);
-        });
+        const { email, password, ...rest } = data;
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                if (Object.keys(rest).length > 0) {
+                    updateProfile(userCredential.user, rest);
+                }
+
+                resolve(userCredential.user);
+            })
+            .catch((err) => {
+                reject(err.message);
+            });
     });
-  
-  export const login = (data) =>
+
+export const login = (data) =>
     new Promise((resolve, reject) => {
-      const { email, password, ...rest } = data;
-  
-      signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          if (Object.keys(rest).length > 0) {
-            updateProfile(userCredential.user, rest);
-          }
-  
-          resolve(userCredential.user);
-        })
-        .catch((err) => {
-          reject(err.message);
-        });
+        const { email, password, ...rest } = data;
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                if (Object.keys(rest).length > 0) {
+                    updateProfile(userCredential.user, rest);
+                }
+
+                resolve(userCredential.user);
+            })
+            .catch((err) => {
+                reject(err.message);
+            });
     });
-  
-  export const logout = () =>
+
+export const logout = () =>
     new Promise((resolve, reject) => {
-      signOut(auth)
-        .then(() => {
-          resolve();
-        })
-        .catch((err) => {
-          reject(err.message);
-        });
+        signOut(auth)
+            .then(() => {
+                resolve();
+            })
+            .catch((err) => {
+                reject(err.message);
+            });
     });
